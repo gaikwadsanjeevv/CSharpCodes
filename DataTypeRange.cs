9456f797455d90dd694de8b51229ef01cf442ef6
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-10-03 10:30:39 -0600

    Create DataTypeRange.cs
    Ranges of different data Types

---
+++ DataTypeRange.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace HelloWorld
+{
+    public class DataTypes
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Byte Values: {0} {1}",byte.MaxValue, byte.MaxValue);
+            Console.WriteLine("Int Values: {0} {1}", int.MaxValue, int.MaxValue);
+            Console.WriteLine("SHort Values: {0} {1}", short.MaxValue, short.MaxValue);
+            Console.WriteLine("Long Values: {0} {1}", long.MaxValue, long.MaxValue);
+            Console.WriteLine("Float Values: {0} {1}", float.MaxValue, float.MaxValue);
+            Console.WriteLine("Double Values: {0} {1}", double.MaxValue, double.MaxValue);
+            Console.WriteLine("Decimal Values: {0} {1}", decimal.MaxValue, decimal.MaxValue);
+            Console.WriteLine("{0} {1}", char.MaxValue, char.MaxValue);
+
+        }
+    }
+}
